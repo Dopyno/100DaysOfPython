bac808ab52b662baad7a7838dd717200ccac022d
Marius Iordan <dopyno@yahoo.com>
2025-06-06 21:00:04 +0100

    if/else

--- fundamentals/dataSet.py
+++ fundamentals/dataSet.py
@@ -1,3 +1,6 @@
 print("Hello world");
 firstName = input("Enter your first name: ")
-print("Welcome " + firstName + "!ma")
+print("Welcome " + firstName + "!")
+height = int(input("Enter your height in cm: "))
+if height > 150 : print("You are tall enaugh to play basketball!");
+else: print("You must wait to grow a little bit");
