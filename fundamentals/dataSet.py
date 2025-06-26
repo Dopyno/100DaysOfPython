a508ab7d526c11973d0433e67560653c800d79c9
Dopyno <dopyno@yahoo.com>
2025-06-26 20:34:40 +0100

    up

--- fundamentals/dataSet.py
+++ fundamentals/dataSet.py
@@ -2,5 +2,8 @@ print("Hello world");
 firstName = input("Enter your first name: ")
 print("Welcome " + firstName + "!")
 height = int(input("Enter your height in cm: "))
-if height > 150 : print("You are tall enaugh to play basketball!");
+if height > 150 : print("You are tall enough to play basketball!");
 else: print("You must wait to grow a little bit");
+
+name = str(input("Please enter you name: "))
+print("Welcome my friend " + name)
