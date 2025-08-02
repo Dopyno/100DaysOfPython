9a40587c72a3ea56ca5ce3ea32bdea06ffa75f5b
Dopyno <dopyno@yahoo.com>
2025-08-02 21:42:10 +0100

    string format

--- fundamentals/dataSet.py
+++
@@ -1,8 +0,0 @@
-#print statement
-firstName = "Marius"
-lastName = "Iordan"
-
-print(firstName + " " + lastName); # normal print 
-print(f"My name is: {firstName} {lastName}"); #fstring to print with place holders
-print(f"""My name is: {firstName} {lastName} and ...
-      I just came back from Romania"""); #fstring to print on multiple lines
---
+++ fundamentals/day_1.py
@@ -0,0 +1,21 @@
+#print statement
+# firstName = "Marius"
+# lastName = "Iordan"
+
+# print(firstName + " " + lastName); # normal print 
+# print(f"My name is: {firstName} {lastName}"); #fstring to print with place holders
+# print(f"""My name is: {firstName} {lastName} and ...
+#       I just came back from Romania"""); #fstring to print on multiple lines
+
+# print("Hello World\nThis is another print line")
+
+# print("Day 1 - Python Print Function")
+# print("""The function is declared like this: 
+#          print('what to print')""")
+
+# print("Hello" + " " + "Angela")
+
+print("Day 1 String Manipulation")
+print("String Concatenation is done with the + sign.")
+print('e.g. print("Hello " + "World")')
+print("New lines can be created with a backslash and n:\\n")
