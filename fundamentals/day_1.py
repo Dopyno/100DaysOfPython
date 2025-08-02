72594f4079fadf8ffa43f02cf5084bdb69cea7d1
Dopyno <dopyno@yahoo.com>
2025-08-02 23:05:28 +0100

    final day 1

--- fundamentals/day_1.py
+++ fundamentals/day_1.py
@@ -15,7 +15,28 @@
 
 # print("Hello" + " " + "Angela")
 
-print("Day 1 String Manipulation")
-print("String Concatenation is done with the + sign.")
-print('e.g. print("Hello " + "World")')
-print("New lines can be created with a backslash and n:\\n")
+# String manipulation
+# print("Day 1 String Manipulation")
+# print("String Concatenation is done with the + sign.")
+# print('e.g. print("Hello " + "World")')
+# print("New lines can be created with a backslash and n:\\n")
+
+# input
+# name = input("What is you name? ")
+# length = len(name) 
+# print(length)
+
+# a = input("a = ")
+# b = input("b = ")
+# temp = a
+# a = b
+# b = temp
+
+# print("a = " + a)
+# print("b = " + b)
+
+print("This is a Band Name Generator.")
+city = input("What is the name of the city that you grow up? \n")
+pet = input("What is you favorite pet? \n")
+
+print(f"Your band name could be {city} {pet}!")
