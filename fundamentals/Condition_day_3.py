eba78f97cc41c94d57160750cab95b2e44b96705
Dopyno <dopyno@yahoo.com>
2025-08-03 21:36:24 +0100

    roller coaster update

--- fundamentals/Condition_day_3.py
+++ fundamentals/Condition_day_3.py
@@ -1,33 +1,46 @@
-# def welcomeMsg():
-#     print("\n===============================")
-#     print("** Welcome to the rollercoaster **")
-#     print("===============================")
+
+#** Rollercoaster park
+
+def welcomeMsg():
+    print("\n===============================")
+    print("** Welcome to the rollercoaster **")
+    print("===============================")
 
 
-# welcomeMsg()
-# height = int(input("\nPlease your height in cm: "))
-# ticket = 0
+welcomeMsg()
+height = int(input("\nPlease your height in cm: "))
+ticket = 0
 
-# if height >= 120: 
-#     print(f"You can ride the rollercoaster!")
-#     age = int(input("Please enter your age: "))
-#     if age < 12:
-#         ticket = 5
-#         print(f"The ticket price for kids is ${ticket}.")
-#     elif age <= 18: 
-#         ticket = 7   
-#         print(f"The ticket price for youth is ${ticket}.")
-#     else:
-#         ticket = 12
-#         print(f"The ticket price is ${ticket}.")
+if height >= 120: 
+    print(f"You can ride the rollercoaster!")
+    age = int(input("Please enter your age: "))
+    if age < 12:
+        ticket = 5
+        print(f"The ticket price for kids is ${ticket}.")
+    elif age <= 18: 
+        ticket = 7   
+        print(f"The ticket price for youth is ${ticket}.")
+    elif age >=45 and age <=55:
+        ticket = 0
+        print(f"The ticket price is ${ticket}.")
+        print(f"Please enjoy your day at the park.")
+        print(f"This is a special offer for midage crisis.")
+    else:
+        ticket = 12
+        print(f"The ticket price is ${ticket}.")
 
-#     photo = input("Do you want photos while you are is rollercoaster just for $3? (Y / N) ")
-#     if photo.casefold() == "y":
-#         ticket += 3
-#     print(f"Total bill is: ${ticket}")
-# else:
-#     print(f"Sorry, you have to grow taller before you can ride!")
+    photo = input("Do you want photos while you are is rollercoaster just for $3? (Y / N) ")
+    if photo.casefold() == "y":
+        if age >=45 and age <=55:
+            ticket += 0
+        else:
+            ticket += 3
+    print(f"Total bill is: ${ticket}")
+else:
+    print(f"Sorry, you have to grow taller before you can ride!")
  
+#** BMI calculator
+
 # print("BMI calculator")
 # height = float(input("Please enter your height is m: "))
 # weight = float(input("Please enter your weight is kg: "))
@@ -47,55 +60,59 @@
 
 # year = int(input("please enter a year to see if is a leap year: "))
 
+#** Leap year
+
 # if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
 #     print(f"The year {year} is a leap year")
 # else:
 #     print(f"The year {year} is not a leap year")
 
-def welcome():
-    print("\n---------------------------------------")
-    print("** Welcome to Python Pizza Delivery! **")
-    print("---------------------------------------\n")
+#**  Python Pizza Calculator
 
-def print_menu():
-    print("=======  PIZZA MENU  =======")
-    print("Small Pizza: $15")
-    print("Medium Pizza: $20")
-    print("Large Pizza: $25\n")
-    print("Pepperoni for Small Pizza: +$2")
-    print("Pepperoni for Medium and Large Pizza: +$3\n")
-    print("Extra cheese for any size of pizza: +$1")
+# def welcome():
+#     print("\n---------------------------------------")
+#     print("** Welcome to Python Pizza Delivery! **")
+#     print("---------------------------------------\n")
 
-def calculate_bill(size, pepperoni, cheese):
-    price = 0
-    if size.casefold() == "s":
-        price += 15
-        print(f"You choose a Small Pizza for: ${price}")
-    elif size.casefold() == "m":
-        price += 20
-        print(f"You choose a Medium Pizza for: ${price}")
-    else:
-        price += 25
-        print(f"You choose a Large Pizza for: ${price}")
+# def print_menu():
+#     print("=======  PIZZA MENU  =======")
+#     print("Small Pizza: $15")
+#     print("Medium Pizza: $20")
+#     print("Large Pizza: $25\n")
+#     print("Pepperoni for Small Pizza: +$2")
+#     print("Pepperoni for Medium and Large Pizza: +$3\n")
+#     print("Extra cheese for any size of pizza: +$1")
 
-    if pepperoni.casefold() == "y":
-        if size.casefold() == "s":
-            price += 2
-            print("Adding pepperoni for: +$2")
-        else:
-            price += 3
-            print("Adding pepperoni for: +$3")
+# def calculate_bill(size, pepperoni, cheese):
+#     price = 0
+#     if size.casefold() == "s":
+#         price += 15
+#         print(f"You choose a Small Pizza for: ${price}")
+#     elif size.casefold() == "m":
+#         price += 20
+#         print(f"You choose a Medium Pizza for: ${price}")
+#     else:
+#         price += 25
+#         print(f"You choose a Large Pizza for: ${price}")
 
-    if cheese.casefold() == "y":
-            price += 1
-            print("Adding pepperoni for: +$1")
-    print(f"Total bill: ${price}")
+#     if pepperoni.casefold() == "y":
+#         if size.casefold() == "s":
+#             price += 2
+#             print("Adding pepperoni for: +$2")
+#         else:
+#             price += 3
+#             print("Adding pepperoni for: +$3")
 
-welcome()
-print_menu()
+#     if cheese.casefold() == "y":
+#             price += 1
+#             print("Adding pepperoni for: +$1")
+#     print(f"Total bill: ${price}")
 
-size = input("What size pizza do you want? S, M or L ")
-pepperoni = input("Do you want pepperoni? (Y / N) ")
-cheese = input("Do you want extra cheese? (Y / N) ")
+# welcome()
+# print_menu()
 
-calculate_bill(size, pepperoni, cheese)
+# size = input("What size pizza do you want? S, M or L ")
+# pepperoni = input("Do you want pepperoni? (Y / N) ")
+# cheese = input("Do you want extra cheese? (Y / N) ")
+
+# calculate_bill(size, pepperoni, cheese)
