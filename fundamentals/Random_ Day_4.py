462bb63c1be5cc6a18b28f17d22a6c2ea049aa71
Dopyno <dopyno@yahoo.com>
2025-08-04 19:59:51 +0100

    random and list

---
+++ fundamentals/Random_ Day_4.py
@@ -0,0 +1,17 @@
+import random 
+import module
+
+# number = random.randint(1, 10)
+# print(number)
+# # print(module.PI)
+# float_number = random.random() * 2
+# print(float_number)
+
+#** Coin game
+
+# coin = random.randint(0, 1)
+# # print(coin)
+# if coin == 1:
+#     print("head")
+# else:
+#     print("tails")
