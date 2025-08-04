02a84e689ba3c7dc586f1f15a0ff5c609dae20ee
Dopyno <dopyno@yahoo.com>
2025-08-04 19:13:10 +0100

    Teasure island

--- fundamentals/Condition_day_3.py
+++ fundamentals/Condition_day_3.py
@@ -1,43 +1,43 @@
 
 #** Rollercoaster park
 
-def welcomeMsg():
-    print("\n===============================")
-    print("** Welcome to the rollercoaster **")
-    print("===============================")
+# def welcomeMsg():
+#     print("\n===============================")
+#     print("** Welcome to the rollercoaster **")
+#     print("===============================")
 
 
-welcomeMsg()
-height = int(input("\nPlease your height in cm: "))
-ticket = 0
+# welcomeMsg()
+# height = int(input("\nPlease your height in cm: "))
+# ticket = 0
 
-if height >= 120: 
-    print(f"You can ride the rollercoaster!")
-    age = int(input("Please enter your age: "))
-    if age < 12:
-        ticket = 5
-        print(f"The ticket price for kids is ${ticket}.")
-    elif age <= 18: 
-        ticket = 7   
-        print(f"The ticket price for youth is ${ticket}.")
-    elif age >=45 and age <=55:
-        ticket = 0
-        print(f"The ticket price is ${ticket}.")
-        print(f"Please enjoy your day at the park.")
-        print(f"This is a special offer for midage crisis.")
-    else:
-        ticket = 12
-        print(f"The ticket price is ${ticket}.")
+# if height >= 120: 
+#     print(f"You can ride the rollercoaster!")
+#     age = int(input("Please enter your age: "))
+#     if age < 12:
+#         ticket = 5
+#         print(f"The ticket price for kids is ${ticket}.")
+#     elif age <= 18: 
+#         ticket = 7   
+#         print(f"The ticket price for youth is ${ticket}.")
+#     elif age >=45 and age <=55:
+#         ticket = 0
+#         print(f"The ticket price is ${ticket}.")
+#         print(f"Please enjoy your day at the park.")
+#         print(f"This is a special offer for midage crisis.")
+#     else:
+#         ticket = 12
+#         print(f"The ticket price is ${ticket}.")
 
-    photo = input("Do you want photos while you are is rollercoaster just for $3? (Y / N) ")
-    if photo.casefold() == "y":
-        if age >=45 and age <=55:
-            ticket += 3
-        else:
-            ticket += 3
-    print(f"Total bill is: ${ticket}")
-else:
-    print(f"Sorry, you have to grow taller before you can ride!")
+#     photo = input("Do you want photos while you are is rollercoaster just for $3? (Y / N) ")
+#     if photo.casefold() == "y":
+#         if age >=45 and age <=55:
+#             ticket += 3
+#         else:
+#             ticket += 3
+#     print(f"Total bill is: ${ticket}")
+# else:
+#     print(f"Sorry, you have to grow taller before you can ride!")
  
 #** BMI calculator
 
@@ -116,3 +116,54 @@ else:
 # cheese = input("Do you want extra cheese? (Y / N) ")
 
 # calculate_bill(size, pepperoni, cheese)
+
+#** Treasure Island Game
+
+def print_logo():
+    print(""" ____________________________________________________________________
+ / \-----     ---------  -----------     -------------- ------    ----
+ \_/__________________________________________________________________/
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~jrei~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  -------
+ \_/__________________________________________________________________/\n""")
+    print("Welcome to the Tresure Island.")
+    print("Your mission is to find the treasure.\n")
+
+
+
+print_logo()
+answer = input("You're at a cross road. Where do you want to go? Type 'left' or 'right' \n")
+
+if answer.casefold() == "left":
+    answer = input("You came to a lake. There is a island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. \n")
+    if answer.casefold() == "wait":
+        answer = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which color do you choose? \n")
+        if answer.casefold() == "yellow":
+            print("\033[1;33m🎉🎉🎉🏆You win!!!!!🏆\033[0m")
+        elif answer.casefold() == "red":
+            print("Burned by fire! Game Over!!")
+        else:
+            print("Game Over!!!!")
+    else:
+        print("Attacked by trout. Game Over!!!")
+else:
+    print("Fall into a hole. Game Over!!!")
