95928e591ffcf15a6c6b04c3e15bc84056c29a71
Dopyno <dopyno@yahoo.com>
2025-08-05 20:38:21 +0100

    rock game

--- fundamentals/Random_ Day_4.py
+++ fundamentals/Random_ Day_4.py
@@ -33,16 +33,82 @@ import module
 # choice = random.randint(0, len(our_list) - 1)
 # print(f"{our_list[choice]} will pay today for the bill!")
 
-row1 = ["🌁", "🌁", "🌁"]
-row2 = ["🌁", "🌁", "🌁"]
-row3 = ["🌁", "🌁", "🌁"]
+# * map Game
 
-our_map = [row1, row2, row3]
+# row1 = ["🌁", "🌁", "🌁"]
+# row2 = ["🌁", "🌁", "🌁"]
+# row3 = ["🌁", "🌁", "🌁"]
 
-print(f"{row1}\n{row2}\n{row3}")
-coord = input("please choose your coordinates: ")
-first = int(coord[0]) - 1
-second = int(coord[1]) - 1
-# print(first, second)
-our_map[second][first] = "❌"
-print(f"{our_map[0]}\n{our_map[1]}\n{our_map[2]}")
+# our_map = [row1, row2, row3]
+
+# print(f"{row1}\n{row2}\n{row3}")
+# coord = input("please choose your coordinates: ")
+# first = int(coord[0]) - 1
+# second = int(coord[1]) - 1
+# # print(first, second)
+# our_map[second][first] = "❌"
+# print(f"{our_map[0]}\n{our_map[1]}\n{our_map[2]}")
+
+# * Rock, Paper Scissor game
+# rock wins against scissor
+# scissor win against paper
+# paper wins against rock
+
+rock = """Rock
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+"""
+paper = """Paper
+     _______
+---'    ____)____
+           ______)
+          _______)
+         _______)
+---.__________)
+"""
+scissor = """Scissor
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+"""
+
+list = [rock, scissor, paper]
+
+player = int(input("What so you choose? Type 1 for Rock, 2 for Paper or 3 for Scissor.\n"))
+computer = random.randint(0, 2)
+
+if player < 0 or player >= 3: 
+    print("You type another number, you lose!!!")
+
+else:
+    print(list[player])
+    print("Computer choose")
+    print(list[computer])
+
+    if computer == 0 and player == 1:
+        print("Computer Win!")
+    elif computer == 1 and player == 2:
+        print("Computer Win!")
+    elif computer == 2 and player == 0:
+         print("Computer Win!")
+    elif computer == player:
+        print("I'ts a draw!")
+
+    if player == 0 and computer == 1:
+        print("You Win!")    
+    elif player == 1 and computer == 2:
+         print("You Win!") 
+    elif player == 2 and computer == 0:
+        print("You Win!") 
+   
+
+
+
+
