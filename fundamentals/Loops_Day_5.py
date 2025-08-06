b44972390448c50473a784c697039ee0061599ca
Dopyno <dopyno@yahoo.com>
2025-08-06 20:42:06 +0100

    password generator

--- fundamentals/Loops_Day_5.py
+++ fundamentals/Loops_Day_5.py
@@ -18,12 +18,75 @@
 
 # *. MAX
 
-numbers = [78, 65, 89, 86, 55, 91, 64, 89, 99, 23, 25]
+# numbers = [78, 65, 89, 86, 55, 91, 64, 89, 99, 23, 25]
 
-max_num = 0
+# max_num = 0
 
-for i in numbers:
-    if i > max_num:
-        max_num = i 
+# for i in numbers:
+#     if i > max_num:
+#         max_num = i 
 
-print(f"The highest score in the class is: {max_num}")
+# print(f"The highest score in the class is: {max_num}")
+
+# ** fizz buzz
+
+# for i in range(1, 101):
+#     if i % 3 == 0 and i % 5 == 0:
+#         print(f"The number {i} is divisible with 3 and 5: FizzBuzz")
+#     elif i % 5 == 0:
+#         print(f"The number {i} is divisible with 5: BUZZZ")
+#     elif i % 3 == 0:
+#         print(f"The number {i} is divisible with 3: FIZZZ")
+#     else:
+#         print(i)
+
+#** Password Generator Project
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+ 
+password = []
+
+for i in range(0, nr_letters):
+    # temp.append(letters[random.randint(0, len(letters) - 1)])
+    # temp.append(random.choice(letters))
+    password.append(random.choice(letters))
+
+for i in range(0, nr_symbols):
+    # temp.append(symbols[random.randint(0, len(symbols) - 1)])
+    # temp.append(random.choice(symbols))
+    password.append(random.choice(symbols))
+
+
+for i in range(0, nr_numbers):
+    # temp.append(numbers[random.randint(0, len(numbers) - 1)])
+    # temp.append(random.choice(numbers))
+    password.append(random.choice(numbers))
+
+y = ""
+for i in range(0, len(password)):
+    y += password[i]
+
+print("Sequence print")
+print(y)
+
+# ** Hard
+
+random.shuffle(password)
+x = ""
+for i in range(0, len(password)):
+    x += password[i]
+
+print("Shuffle print")
+print(x)
+
+# for i in range(0, len(temp)):
+#     x = random.shuffle(temp[i])
+#     print(x)
+
