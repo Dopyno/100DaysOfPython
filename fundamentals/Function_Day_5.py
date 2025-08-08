d7748877aa4bceec2b84250ec069d24b1a46cddb
Dopyno <dopyno@yahoo.com>
2025-08-08 20:39:49 +0100

    maze and hudle 3, 4

--- fundamentals/Function_Day_5.py
+++ fundamentals/Function_Day_5.py
@@ -1,3 +1,6 @@
+
+# ** Reborg 3
+
 def turn_around():
     turn_left()
     turn_left()
@@ -24,4 +27,62 @@ while not at_goal():
         else:
             break
     
+
+# ** REborg 4
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
     
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    while front_is_clear():
+        move()
+    turn_left()
+        
+    
+while not at_goal():
+        if wall_in_front():
+            jump()
+        else:
+            move()
+    
+# ** Maze  
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def jump():
+    turn_left()
+    while wall_on_right():
+        if wall_in_front():
+            turn_left()
+        else:
+            move()
+    if right_is_clear():
+        turn_right()
+    else:
+        turn_left()
+    while front_is_clear():
+        if at_goal():
+            break
+        move()
+    turn_right()
+    if wall_in_front() and wall_on_right():
+        turn_left()
+        move()
+        
+    
+while not at_goal():
+        if wall_in_front():
+            jump()
+        else:
+            move()
