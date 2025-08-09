d8c0ec9cb85178ced1ec762f134b1df1d7bc3b5f
Dopyno <dopyno@yahoo.com>
2025-08-09 19:20:31 +0100

    update maize

--- fundamentals/Function_Day_5.py
+++ fundamentals/Function_Day_5.py
@@ -76,3 +76,19 @@ while not at_goal():
             turn_left()
         else:
             move()
+
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
