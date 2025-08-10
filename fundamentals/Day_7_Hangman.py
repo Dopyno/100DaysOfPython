219aac73445ced3290d20e78bf4d27be29f21f71
Dopyno <dopyno@yahoo.com>
2025-08-10 08:06:32 +0100

    up1

--- fundamentals/Day_7_Hangman.py
+++ fundamentals/Day_7_Hangman.py
@@ -7,22 +7,25 @@
 import random
 
 word_list = ["camel", "elephant", "fireman"]
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
 
 selected_word = random.choice(word_list)
-
+size = len(selected_word)
 user_raw_letter = ""
+user_letter = ""
+count = 0
 
-while user_raw_letter == "":
-    user_raw_letter = input("Please choose a letter: ")
-    user_letter = user_raw_letter.lower()
-
-    for x in selected_word:
+while count < 10:
+    for x in range(size):
         if user_letter == x:
             print(f"{x}", end=" ")
         else:
-            print("_", end=" ")
+                print("_", end=" ")
 
-
+    user_raw_letter = input("\nPlease choose a letter: ")
+    user_letter = user_raw_letter.lower()
+    count += 1
 
 
 
