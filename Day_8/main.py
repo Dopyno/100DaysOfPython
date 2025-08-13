803d414ef77e3e2bcca1e0ade7353ed3afc048cb
Dopyno <dopyno@yahoo.com>
2025-08-13 21:20:19 +0100

    up

---
+++ Day_8/main.py
@@ -0,0 +1,13 @@
+def greet():
+    print("hello")
+    print("How do you do?")
+    print("How is the weather today?")
+
+greet()
+
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}")
+
+greet_with_name("Angela")
