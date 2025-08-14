2c4ff30e3b9670aa5a3e2d64004aace26ce3e17b
Dopyno <dopyno@yahoo.com>
2025-08-14 19:00:38 +0100

    cipher

---
+++ Caesar Cipher/main.py
@@ -0,0 +1,5 @@
+from alphabet import alphabet
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt\n")
+message = input("Type your message: \n")
+shift = int(input("Type the shift number:\n"))
