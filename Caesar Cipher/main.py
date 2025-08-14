586d6f562170e44e40f230a0f074121e85675726
Dopyno <dopyno@yahoo.com>
2025-08-14 20:55:27 +0100

    update

--- Caesar Cipher/main.py
+++ Caesar Cipher/main.py
@@ -2,4 +2,14 @@ from alphabet import alphabet
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt\n")
 message = input("Type your message: \n")
-shift = int(input("Type the shift number:\n"))
+shift = int(input("Type the shift number: "))
+
+size = len(message)
+
+def encrypt(message, shift):
+    for i in range(size):
+       temp = alphabet.index(message[i])
+       new = temp + shift - 1
+       print(alphabet[new], end="")
+
+encrypt(message, shift)
