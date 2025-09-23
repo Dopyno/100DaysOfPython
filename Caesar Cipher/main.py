a44a95e3ad828aaa8d355c9e5e5140aee34dd4e9
Dopyno <dopyno@yahoo.com>
2025-09-23 13:41:24 +0100

    encrypt

--- Caesar Cipher/main.py
+++ Caesar Cipher/main.py
@@ -6,14 +6,25 @@ shift = int(input("Type the shift number: "))
 
 # size = len(message)
 
-def encrypt(message, shift):
+
+def encrypt(plain_message, shift_amount):
     cipher_text = ""
-    for letter in message:
-       cipher_text = ""
+    for letter in plain_message:
        temp = alphabet.index(letter)
-       new_position = temp + shift
+       new_position = temp + shift_amount
        new_letter = alphabet[new_position]
        cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
+    print(f"The encrypted text is {cipher_text}")
+def decrypt(cipher_text, shift_amount):
+    encode_text = ""
+    for letter in cipher_text:
+        temp = alphabet.index(letter)
+        new_position = temp - shift_amount
+        new_letter = alphabet[new_position]
+        encode_text += new_letter
+    print(f"The encoded text is {encode_text}")
 
-encrypt(message, shift)
+if direction == "encode":
+    encrypt(plain_message=message, shift_amount=shift)
+elif direction == "decode":
+    decrypt(cipher_text = message, shift_amount=shift)
