6f1f0d93c498b594ef4f54d9c4000d9554bc967f
Dopyno <dopyno@yahoo.com>
2025-09-25 22:43:07 +0100

    finish day 9

--- auction/main.py
+++ auction/main.py
@@ -1,7 +1,7 @@
 import os
 from art import art_logo
 
-continue_game = True
+bidding_finished = False
 import os
 
 def clear():
@@ -13,15 +13,29 @@ print("\n")
 print("Welcome to the secret auction!")
 players = {}
 
-while continue_game:
-    name = input("What is you name: ")
-    bid = int(input("What's your bid: £"))
+def find_highest_bid(bidding_record):
+    highest_bid = 0
+    winner = ""
+    # bidding_record = {"angela": 123, "marius": 333}
+    for bidder in players:
+        big_amount = players[bidder]
+        if big_amount > highest_bid:
+            highest_bid = big_amount
+            winner = bidder
+    print(f"The winner is {winner}, with a bid of £{highest_bid}")
+
+
+while not bidding_finished:
+    name = input("What is you name?: ")
+    bid = int(input("What's your bid?: £"))
     players[name] = bid
-    morePlayers = input("Are there any others biders, (Yes / No): ")
+    morePlayers = input("Are there any others biders, (Yes / No): \n")
     if morePlayers.lower() == "no":
-        continue_game = False
-    clear()
+        bidding_finished = True
+        find_highest_bid(players)
+    elif morePlayers.lower() == "yes":
+        clear() 
 
-highest_bid = max(players, key=players.get)
+# highest_bid = max(players, key=players.get)
 
-print(f"The winner is {highest_bid}, with a bid of £{players[highest_bid]}")
+# print(f"The winner is {highest_bid}, with a bid of £{players[highest_bid]}")
