b0af092f577b9e17d983ec9c483c7ee6ac04b57f
Dopyno <dopyno@yahoo.com>
2025-09-26 20:53:21 +0100

    ret

--- Day_10_Return_Function/main.py
+++ Day_10_Return_Function/main.py
@@ -1,10 +1,12 @@
 # function with output
 
 def format_name(first_name, last_name):
-  print(f"{first_name.title()} {last_name.title()}")
+  if first_name == "" and last_name == "":
+    print("Warning! You need to provide you name!")
+    return
+#   print(f"{first_name.title()} {last_name.title()}")
   return f"{first_name.title()} {last_name.title()}"
 
-name = format_name("marius", "iordan")
-
-print(name)
+# name = format_name("marius", "iordan")
+print(format_name(input("What is your first name: "), input("What is your last name: ")))
   
