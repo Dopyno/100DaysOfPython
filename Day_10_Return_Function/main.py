cc1b9a87c89580a12f63c83fa47759c0b91832dc
Dopyno <dopyno@yahoo.com>
2025-09-28 17:42:38 +0100

    update

--- Day_10_Return_Function/main.py
+++ Day_10_Return_Function/main.py
@@ -14,26 +14,27 @@
 
 
 def days_in_month(year, month):
-   def is_leap(year):
-    if year % 4 == 0:
-        if year % 100 == 0:
-            if year % 400 == 0:
-              print("Leap year!")
-              leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-              return leap
-            else: 
-              print("Not a leap year!")
-              not_leap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-              return not_leap
-        else: 
-          print("Not a leap year!")
-          not_leap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-          return not_leap
-    else: 
-        print("Not a leap year!")
-        not_leap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-        return not_leap
-
+    def is_leap(year):
+      if year % 4 == 0:
+          if year % 100 == 0:
+              if year % 400 == 0:
+                return True
+              else: 
+                return False
+          else: 
+            return True
+      else: 
+            return False
+    # Days in months for regular and leap years
+    leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    not_leap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year):
+        return leap[month-1]
+    else:
+        return not_leap[month-1]
 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
+
+
+print(f"Number of days: {days_in_month(year, month)}")
