fb437574de166ab5d5ff1360aa1dd8ce98f3abd1
Dopyno <dopyno@yahoo.com>
2025-09-28 08:52:31 +0100

    update

--- Day_10_Return_Function/main.py
+++ Day_10_Return_Function/main.py
@@ -1,14 +1,39 @@
-# function with output
+# # function with output
 
-def format_name(first_name, last_name):
-  if first_name == "" and last_name == "":
-    print("Warning! You need to provide you name!")
-    return
-#   print(f"{first_name.title()} {last_name.title()}")
-  return f"{first_name.title()} {last_name.title()}"
+# def format_name(first_name, last_name):
+#   if first_name == "" or last_name == "":
+#     print("Warning! You need to provide you name!")
+#     return
+# #   print(f"{first_name.title()} {last_name.title()}")
+#   return f"{first_name.title()} {last_name.title()}"
 
-# name = format_name("marius", "iordan")
-print(format_name(input("What is your first name: "), input("What is your last name: ")))
+# # name = format_name("marius", "iordan")
+# print(format_name(input("What is your first name: "), input("What is your last name: ")))
   
 
-print()
+
+
+def days_in_month(year, month):
+   def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+              print("Leap year!")
+              leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+              return leap
+            else: 
+              print("Not a leap year!")
+              not_leap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+              return not_leap
+        else: 
+          print("Not a leap year!")
+          not_leap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+          return not_leap
+    else: 
+        print("Not a leap year!")
+        not_leap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        return not_leap
+
+
+year = int(input("Enter a year: "))
+month = int(input("Enter a month: "))
