23d5c6561c023da083c55daaaab0fe3e86389898
Dopyno <dopyno@yahoo.com>
2025-09-28 18:35:22 +0100

    doc string

--- Day_10_Return_Function/main.py
+++ Day_10_Return_Function/main.py
@@ -1,37 +1,42 @@
 # # function with output
 
-# def format_name(first_name, last_name):
-#   if first_name == "" or last_name == "":
-#     print("Warning! You need to provide you name!")
-#     return
-# #   print(f"{first_name.title()} {last_name.title()}")
-#   return f"{first_name.title()} {last_name.title()}"
+def format_name(first_name, last_name):
+  """Take a first and last name and format it
+  to return the title case version."""
+  if first_name == "" or last_name == "":
+    print("Warning! You need to provide you name!")
+    return
+#   print(f"{first_name.title()} {last_name.title()}")
+  return f"{first_name.title()} {last_name.title()}"
 
-# # name = format_name("marius", "iordan")
-# print(format_name(input("What is your first name: "), input("What is your last name: ")))
+# name = format_name("marius", "iordan")
+print(format_name(input("What is your first name: "), input("What is your last name: ")))
   
+def is_leap(year):
+  """Return if the year is leap or not"""
+  if year % 4 == 0:
+      if year % 100 == 0:
+          if year % 400 == 0:
+            return True
+          else: 
+            return False
+      else: 
+        return True
+  else: 
+        return False
 
 
 
 def days_in_month(year, month):
-    def is_leap(year):
-      if year % 4 == 0:
-          if year % 100 == 0:
-              if year % 400 == 0:
-                return True
-              else: 
-                return False
-          else: 
-            return True
-      else: 
-            return False
+    """Return the day in month if the year is leap."""
+    # if month > 12 and month < 1:
+    #    return "Invalid month"
     # Days in months for regular and leap years
     leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    not_leap = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    if is_leap(year):
-        return leap[month-1]
-    else:
-        return not_leap[month-1]
+    if is_leap(year) and month == 2:
+        return 29
+    return leap[month - 1]
+  
 
 year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
