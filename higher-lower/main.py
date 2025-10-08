9658e05f70918b1e486f77ab2f69e9d2439acefe
Dopyno <dopyno@yahoo.com>
2025-10-08 21:13:58 +0100

    final

--- higher-lower/main.py
+++ higher-lower/main.py
@@ -8,50 +8,51 @@ def generate(data_set):
 
 
 def refine_data(dataSet1):
-    name = dataSet1['name']
+    name = dataSet1["name"]
     description = dataSet1["description"]
     country = dataSet1["country"]
     followers = dataSet1["follower_count"]
-    
+
     return name, description, country, followers
 
 
 def play_game():
     play = True
     score = 0
-    clear_screen()
-    print(logo)
+    # clear_screen()
+    # print(logo)
     version_a = refine_data(generate(data))
+    version_b = refine_data(generate(data))
 
     while play:
-        # clear_screen()
-        # print(logo)
+        clear_screen()
+        print(logo)
         # version_a = refine_data(generate(data))
-        
-        version_b = refine_data(generate(data))
+
         print(f"Score: {score}")
 
-
-
-        print(f"Compare: {version_a[0]}, {version_a[1]}, {version_a[2]}, {version_a[3]}")
+        print(
+            f"Compare: A => {version_a[0]}, {version_a[1]}, {version_a[2]}"
+        )
         print(vs)
-        print(f"Compare: {version_a[0]}, {version_a[1]}, {version_a[2]}, {version_a[3]}")
-
-
-
+        print(
+            f"Compare: B => {version_b[0]}, {version_b[1]}, {version_b[2]}"
+        )
 
         user = input("Who has more followers? 'A' or 'B': ").lower()
 
-        if a >= b and user == "a":
+        if version_a[3] >= version_b[3] and user == "a":
             score += 1
             print(f"Great, your current score {score}")
-        elif a <= b and user == "b":
+            version_b = refine_data(generate(data))
+        elif version_a[3] <= version_b[3] and user == "b":
             score += 1
             print(f"You'r right, your current score {score}")
+            version_a = refine_data(generate(data))
         else:
             print(f"Game over, your final score: {score}")
             play = False
-    if input("Do you want to continue? (Y or N): ").lower() == 'y':
+    if input("Do you want to continue? (Y or N): ").lower() == "y":
         play_game()
     else:
         print(f"Game over, your final score: {score}")
