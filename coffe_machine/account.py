fc1761a45bf48fab61f99c000b0a27c99df95a1d
Dopyno <dopyno@yahoo.com>
2025-10-14 19:22:54 +0100

    receipt

--- coffe_machine/account.py
+++ coffe_machine/account.py
@@ -1,3 +1,6 @@
 money = {
-
+    "pounds": 0
 }
+
+receipt = [0]
+total_receipt = 0
--- coffe_machine/main.py
+++ coffe_machine/main.py
@@ -3,6 +3,9 @@ from menu import MENU, resources
 from account import money
 from clear import clear_screen
 import time
+from account import money, receipt
+import sys
+
 
 def make_coffee(drink):
     print("\n")
@@ -18,13 +21,18 @@ def make_coffee(drink):
         time.sleep(1)
     for item in MENU[drink]["ingredients"]:
         resources[item] -= MENU[drink]["ingredients"][item]
+    money["pounds"] += MENU[drink]["cost"]
     print("Your coffee has been prepared!")
     print("Please take your drink 🥤!")
+
 
 def print_report():
     print("Resources: ")
     for item in resources:
         print(f"{item}: {resources[item]}")
+    for item in money:
+        print(f"Total money: {money[item]}")
+
 
 def print_menu():
     print("\n=======  MENU  =======")
@@ -35,7 +43,9 @@ def print_menu():
     print("==  Technician only  ==")
     print("5. Report")
     print("6. Refill")
+    print("7. Cash")
     print("======================")
+
 
 def refill():
     print("Please enter the values in ml and g: ")
@@ -44,30 +54,67 @@ def refill():
         resources[item] += value
     print("All has been added successfully!✅")
 
-print("Starting the coffee machine...♨️")
-time.sleep(2)
-print("Coffe machine is ready!")
-time.sleep(1)
-print(art.logo)
-print_menu()
-user_action = input("Please select your coffee: (espresso / double / chocolate / latte)")
-match user_action:
-    case "espresso":
-        print(art.espresso)
-        make_coffee(user_action)
-    case "double":
-        print(art.double)
-        make_coffee(user_action)
-    case "chocolate":
-        print(art.chocolate)
-        make_coffee(user_action)
-    case "latte":
-        print(art.latte)
-        make_coffee(user_action)
-    case "report": print_report()
-    case "refill":
-        refill()
-        print_report()
+
+def cash_out():
+    if (input("Do you want to withdrew all the money (Y / N): ")).lower() == "y":
+        money["pounds"] = 0
+        print("Processing", end="")
+        for _ in range(5):
+            time.sleep(0.5)
+            print(".", end="")
+            sys.stdout.flush()
+        print("\nThe money has been withdraw successfully!✅")
+    else:
+        print("You have been transferred to the main menu!")
+        return
 
 
+def starting_coffee_machine():
+    print("Starting the coffee machine♨️ ", end="")
+    for _ in range(5):
+        time.sleep(0.5)
+        print(".", end="")
+        sys.stdout.flush()
+    print("\nCoffee machine is ready!")
+    time.sleep(1)
+    print(art.logo)
 
+
+def print_receipt():
+    print("=====  Receipt  =====")
+    for i in receipt:
+        print(i)
+    print("=====================")
+
+def start():
+    starting_coffee_machine()
+    is_on = True
+
+    while is_on:
+        print_menu()
+        user_action = input(
+            "Please select your coffee: (espresso / double / chocolate / latte or Exit): "
+        ).lower()
+        match user_action:
+            case "espresso":
+                print(art.espresso)
+                make_coffee(user_action)
+            case "double":
+                print(art.double)
+                make_coffee(user_action)
+            case "chocolate":
+                print(art.chocolate)
+                make_coffee(user_action)
+            case "latte":
+                print(art.latte)
+                make_coffee(user_action)
+            case "report":
+                print_report()
+            case "refill":
+                refill()
+                print_report()
+            case "cash":
+                cash_out()
+        if(user_action) == "exit":
+
+    clear_screen()
