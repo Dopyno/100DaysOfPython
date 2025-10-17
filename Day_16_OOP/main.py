fad7773788ae61600323517deb8481263aef898f
Dopyno <dopyno@yahoo.com>
2025-10-17 21:45:30 +0100

    up

--- Day_16_OOP/main.py
+++ Day_16_OOP/main.py
@@ -1,7 +1,23 @@
-from turtle import Turtle, Screen
-import tkinter
+from turtle import *
 
-timmy =  Turtle()
+timmy = Turtle()
+jim = Turtle()
+jim.shape("turtle")
+
+print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+timmy.pencolor("blue")
+move = 100
+for _ in range(5):
+    timmy.forward(move)
+    timmy.left(90)
+    timmy.forward(move)
+    timmy.left(90)
+    timmy.forward(move)
+    timmy.left(90)
+    timmy.forward(move)
+    move -= 10
 
 my_screen = Screen()
 print(my_screen.canvheight)
